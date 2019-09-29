ee716e5e03a7d9e119c542caa741c749e6870952
yingshaoxo <yingshaoxo@gmail.com>
2019-09-29 20:43:26 +0800

    update time table

--- create_a_time_table.py
+++ create_a_time_table.py
@@ -1,11 +1,19 @@
 import datetime
 
+task_num = 0
 for i in range(27):
     today = datetime.date.today()
     new_day = today + datetime.timedelta(days=i)
     if i % 3 == 0:
         print('_'*3)
-    print("* " + str(new_day.month) + "月" + str(new_day.day) + "日: ")
+        print()
+        task_num += 1
+        print(f"### Task{task_num}: ")
+        print()
+    print("" + str(new_day.month) + "月" + str(new_day.day) + "日: ")
+    print("* yingshaoxo: ")
+    print("* 孙雅斌: ")
+    print("* 刘露: ")
     print()
 
 new_day = today + datetime.timedelta(days=i)
