1519233c87c457fc933c5fb71999c5de871ea865
yingshaoxo <yingshaoxo@gmail.com>
2019-10-03 21:00:28 +0800

    add black_line finder; update readme.md

---
+++ OpenMV/grayscale.py
@@ -0,0 +1,57 @@
+# find_black_lines - By: yingshaoxo - Wed Oct 2 2019
+
+# 注意：线条检测是通过使用霍夫变换完成的：
+# http://en.wikipedia.org/wiki/Hough_transform
+# 请阅读以上关于“theta”和“rho”的更多信息。
+
+# find_lines（）找到无限长度的线。使用find_line_segments（）
+# 来查找非无限线。
+
+import time
+import image
+import sensor
+
+sensor.reset()
+sensor.set_pixformat(sensor.RGB565)  # grayscale is faster
+#sensor.set_pixformat(sensor.GRAYSCALE)  # grayscale is faster
+sensor.set_framesize(sensor.QQVGA)  # QVGA: 320x240, QQVGA: 160x120
+sensor.skip_frames(time=2000)
+
+enable_lens_corr = True  # turn on for straighter lines...
+
+# for getting screen resolution
+screen_width = sensor.width()
+screen_height = sensor.height()
+
+# white and black threshold
+THRESHOLD = 14
+
+# for reducing noises in image
+EROSION_SIZE = 2
+
+# for infinite lines
+horizontal_range = 30
+vertical_range = 30
+theta = 0
+# take 3 points at that line, if at least 1 point is black, we say it's a black line
+
+def find_lines(img):
+    grayscale_img = img.to_grayscale(copy=True)
+    binary_img = img.binary([(0,THRESHOLD)], invert=True, copy=False)
+    binary_img.erode(EROSION_SIZE)
+    for l in img.find_lines(threshold=9000, theta_margin=25, rho_margin=25):
+        # theta_margin: it should change according to the angle-change a drone will make when it's in flying
+        # rho_margin: the larger rho is, the thicker the line
+        theta = l.theta()
+        if (179-horizontal_range <= theta <= 179) or (0 <= theta <= 0+horizontal_range):
+            img.draw_line(l.line(), color=(0, 255, 0))
+        elif (90-vertical_range <= theta <= 90+vertical_range):
+            img.draw_line(l.line(), color=(255, 0, 0))
+
+while(True):
+    img = sensor.snapshot()
+
+    if enable_lens_corr:
+        img.lens_corr(1.8)  # for 2.8mm lens...
+
+    find_lines(img)
--- OpenMV/main.py
+++ OpenMV/main.py
@@ -1,20 +1,4 @@
-# Untitled - By: yingshaoxo - Wed Oct 2 2019
-
-"""
-import sensor, image, time
-
-sensor.reset()
-sensor.set_pixformat(sensor.RGB565)
-sensor.set_framesize(sensor.VGA)
-sensor.skip_frames(time = 2000)
-
-while(True):
-    img = sensor.snapshot()
-"""
-# 识别直线例程
-#
-# 这个例子展示了如何在图像中查找线条。对于在图像中找到的每个线对象，
-# 都会返回一个包含线条旋转的线对象。
+# find_black_lines - By: yingshaoxo - Wed Oct 2 2019
 
 # 注意：线条检测是通过使用霍夫变换完成的：
 # http://en.wikipedia.org/wiki/Hough_transform
@@ -26,47 +10,83 @@ while(True):
 import time
 import image
 import sensor
-enable_lens_corr = False  # turn on for straighter lines...
-
 
 sensor.reset()
 sensor.set_pixformat(sensor.RGB565)  # grayscale is faster
-sensor.set_framesize(sensor.QVGA)
+#sensor.set_pixformat(sensor.GRAYSCALE)  # grayscale is faster
+sensor.set_framesize(sensor.QQVGA)  # QVGA: 320x240, QQVGA: 160x120
 sensor.skip_frames(time=2000)
-clock = time.clock()
 
-# 所有的线对象都有一个`theta（）`方法来获取它们的旋转角度。
-# 您可以根据旋转角度来过滤线条。
+enable_lens_corr = True  # turn on for straighter lines...
 
-min_degree = 0
-max_degree = 179
+# white and black threshold
+THRESHOLD = 14
 
-# 所有线段都有 `x1()`, `y1()`, `x2()`, and `y2()` 方法来获得他们的终点
-# 一个 `line()` 方法来获得所有上述的四个元组值，可用于 `draw_line()`.
+# for getting screen resolution
+screen_width = sensor.width()
+screen_height = sensor.height()
+
+# for line segments
+line_segment_merging_distance = screen_width // 8
+max_theta_difference = 15
+# take 3 points at that line, if at least 2 point is black, we say it's a black line
+
+# for infinite lines
+horizontal_range = 30
+vertial_range = 30
+theta = 0
+# take 3 points at that line, if at least 1 point is black, we say it's a black line
+
+def is_a_black_line(line, img):
+    x1, y1, x2, y2 = line.line()
+    point1 = (int((1/4)*x2 + (3/4)*x1), int((1/4)*y2 + (3/4)*y1))
+    point2 = (int((2/4)*x2 + (2/4)*x1), int((2/4)*y2 + (2/4)*y1))
+    point3 = (int((3/4)*x2 + (1/4)*x1), int((3/4)*y2 + (1/4)*y1))
+    points = [point1, point2, point3]
+
+    total_points = len(points)
+    black_points = 0
+    for x,y in points:
+        value = img.get_pixel(x, y)
+        if (value != None):
+            value = image.rgb_to_grayscale(value)
+            if (value <= THRESHOLD):
+                black_points += 1
+
+    if black_points / total_points > 0.6:
+    #if black_points > 0:
+        return True
+    else:
+        return False
+
+def find_lines(img):
+    for l in img.find_line_segments(merge_distance=line_segment_merging_distance, max_theta_diff=5):
+    #for l in img.find_lines(threshold=7000, theta_margin=25, rho_margin=25):
+        if (is_a_black_line(l, img)):
+            theta = l.theta()
+            if (179-horizontal_range <= theta <= 179) or (0 <= theta <= 0+horizontal_range):
+                img.draw_line(l.line(), color=(0, 255, 0))
+            elif (90-vertial_range <= theta <= 90+vertial_range):
+                img.draw_line(l.line(), color=(255, 0, 0))
+
+        """
+        the parent_line object:
+        {"x1":226, "y1":0, "x2":226, "y2":239, "length":239, "magnitude":8778, "theta":0, "rho":226}
+
+        the method of that parent_line:
+        ['line', 'x1', 'y1', 'x2', 'y2', 'length', 'magnitude', 'theta', 'rho']
+        > use it by calling it, like line.length()
+
+        the parent_line object:
+        (202, 0, 194, 239)
+
+        the range of theta is (0, 179)
+        """
 
 while(True):
-    clock.tick()
     img = sensor.snapshot()
+
     if enable_lens_corr:
         img.lens_corr(1.8)  # for 2.8mm lens...
 
-    # `threshold` controls how many lines in the image are found. Only lines with
-    # edge difference magnitude sums greater than `threshold` are detected...
-
-    # `threshold`控制从霍夫变换中监测到的直线。只返回大于或等于阈值的
-    # 直线。应用程序的阈值正确值取决于图像。注意：一条直线的大小是组成
-    # 直线所有索贝尔滤波像素大小的总和。
-
-    # `theta_margin`和`rho_margin`控件合并相似的直线。如果两直线的
-    # theta和ρ值差异小于边际，则它们合并。
-
-    for l in img.find_lines(threshold=7000, theta_margin=25, rho_margin=25):
-        if (min_degree <= l.theta()) and (l.theta() <= max_degree):
-            img.draw_line(l.line(), color=(255, 0, 0))
-            # print(l)
-
-    print("FPS %f" % clock.fps())
-
-# About negative rho values:
-#
-# A [theta+0:-rho] tuple is the same as [theta+180:+rho].
+    find_lines(img)
